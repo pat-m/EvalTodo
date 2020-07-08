9e5c30f8332405118b55d98959d6865c9d453272
Patrick MENGUAL <patrick.mengual@campus-igs-toulouse.fr>
2020-07-08 14:46:44 +0200

    update project

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,6 +1,12 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EvalTodo.ViewModel
 {
@@ -19,20 +25,77 @@ namespace EvalTodo.ViewModel
     public class MainViewModel : ViewModelBase
     {
         public ObservableCollection<Task> Tasks { get; set; }
+        public int TasksDone { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand DeleteAllCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public Task NewTask {get; set;}
+        private String _Name {get; set;}
+        public String Name
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+                RaisePropertyChanged();
+            }
+        }
+        private DateTime _SelectedDate;
+        public DateTime SelectedDate {
+            get { 
+                return this._SelectedDate; 
+            }
+            set {
+                this._SelectedDate = value;
+                RaisePropertyChanged(); 
+            }
+        }
 
         public MainViewModel()
         {
             this.Tasks = new ObservableCollection<Task>();
-
+            this.SelectedDate = DateTime.Now;
+            this.Name = "";
+            //this.Tasks.CollectionChanged += refreshTasksDone;
+            AddCommand = new RelayCommand(() => AddClick());
+            DeleteAllCommand = new RelayCommand(() => DeleteClickAll());
+            DeleteCommand = new RelayCommand(() => DeleteClick());
             if (IsInDesignMode) {
                 this.Tasks.Add(new Task { Name = "Tache1", DateTask = DateTime.Now, IsDone = false });
                 this.Tasks.Add(new Task { Name = "Tache2", DateTask = DateTime.Now, IsDone = true });
             }
-           
             else {
                 this.Tasks.Add(new Task { Name = "Tache1", DateTask = DateTime.Now, IsDone = false });
                 this.Tasks.Add(new Task { Name = "Tache2", DateTask = DateTime.Now, IsDone = true });
             }
         }
+
+        private void AddClick()
+        {
+            Task task = new Task();
+            task.DateTask = this.SelectedDate;
+            task.Name = this.Name;
+            task.IsDone = false;
+            this.Tasks.Add(task);
+            this.Name = "";
+            this.SelectedDate = DateTime.Now;
+        }
+
+        private void DeleteClickAll()
+        {
+            this.Tasks.Clear();
+        }
+
+        private void DeleteClick() { 
+        }
+
+        /*private void refreshTasksDone(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.TasksDone = this.Tasks.Where(t => t.IsDone == true).Count();
+        }
+        */
     }
 }
